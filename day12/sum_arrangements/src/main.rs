4b6dc0de0f54a0d3af5d5f1ef00c140658564293
Elsa Keirouz <elsa.keirouz@epita.fr>
2023-12-13 02:11:46 +0100

    day12: cleaned up

--- day12/sum_arrangements/src/main.rs
+++ day12/sum_arrangements/src/main.rs
@@ -57,7 +57,7 @@ fn process_p2(input: &str) -> (Vec<Vec<char>>, Vec<Vec<usize>>) {
 }
 
 fn parse_input() -> String {
-    fs::read_to_string(String::from("../input.txt")).expect("Could not read file")
+    fs::read_to_string(String::from("../mini_input.txt")).expect("Could not read file")
 }
 
 fn arrange(rec: &Vec<char>, groups: &Vec<usize>) -> usize {
@@ -88,10 +88,10 @@ fn arrange(rec: &Vec<char>, groups: &Vec<usize>) -> usize {
         while i < count && rec[i] != '.' {
             i += 1;
         }
-        if i < rec.len() && (i < count || rec[i] == '#') {
-            return 0;
-        }
         if i < rec.len() {
+            if i < count || rec[i] == '#' {
+                return 0;
+            }
             return arrange(&rec[i + 1..].to_vec(), &groups[1..].to_vec());
         }
         arrange(&rec[i..].to_vec(), &groups[1..].to_vec())
